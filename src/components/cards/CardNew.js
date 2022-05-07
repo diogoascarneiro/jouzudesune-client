95517044025c40f72ceb35a9de16eac7a021f8e3
Diogo <diogoascarneiro@hotmail.com>
2022-05-07 12:47:35 +0100

    added react hover package

--- src/components/cards/CardNew.js
+++ src/components/cards/CardNew.js
@@ -1,9 +1,8 @@
 import { useState, useEffect } from "react";
 import { getCard } from "../../api";
 import { AudioButton } from "../global/AudioButton";
-import ReactHover, { Trigger, Hover } from "react-hover";
 
-export const CardBack = ({ id, moveToNextCard }) => {
+export const CardNew = ({ id, moveToNextCard }) => {
   /* Not going to implement something like DOMPurifier to sanitize html for dangerouslySetInnerHtml
     for now since this is mostly for practicing react and I set the data on the db myself, but
     I'm leaving this here for future reference if I have the time to implement it later.  
@@ -27,12 +26,6 @@ export const CardBack = ({ id, moveToNextCard }) => {
     return congratulations[Math.floor(Math.random() * congratulations.length)];
   };
 
-  const optionsCursorTrueWithMargin = {
-    followCursor: true,
-    shiftX: 20,
-    shiftY: 0
-  };
-
   return (
     <div className="card lg:card-side bg-neutral shadow-xl">
       <div className="flex justify-center flex-col lg:items-center px-20">
@@ -50,17 +43,10 @@ export const CardBack = ({ id, moveToNextCard }) => {
         <div>
           <b>Example sentence: </b>
           <br />
-          <ReactHover
-                  options={optionsCursorTrueWithMargin}>
-                  <Trigger type='trigger'>
-                  <h2
+          <h2
             dangerouslySetInnerHTML={{ __html: card.exampleSentence }}
           ></h2>
-                  </Trigger>
-                  <Hover type='hover'>
-                  <div className="mb-2">(<span dangerouslySetInnerHTML={{ __html: card.exampleInKana }}></span>)</div>
-                                      </Hover>
-                  </ReactHover>
+          <div className="mb-2">(<span dangerouslySetInnerHTML={{ __html: card.exampleInKana }}></span>)</div>
          <div className="mb-2">{card.exampleTranslation}</div> 
           <AudioButton src={`/media/${card.exampleAudio}`}>Listen</AudioButton>
         </div>
